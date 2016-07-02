f2767cf4d4982e146e0f5002008cc4e29839fc06
andreiSaw <ias96@list.ru>
2016-07-02 15:20:46 +0300

    initial commit
    15 20

---
+++ main.py
@@ -0,0 +1,67 @@
+# import the Flask class from the flask module
+from flask import Flask, render_template, redirect, url_for, request, session, flash
+from functools import wraps
+
+# create the application object
+app = Flask(__name__)
+# config
+app.secret_key = "developers"
+
+
+# use decorators to link the function to a url
+@app.route('/')
+def home():
+    return redirect(url_for('welcome'))
+
+
+# login required decorator
+def login_required(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return f(*args, **kwargs)
+        else:
+            flash('You need to login first.')
+            return redirect(url_for('login'))
+
+    return wrap
+
+
+# use decorators to link the function to a url
+@app.route('/welcome')
+def welcome():
+    return render_template('welcome.html')  # render a template
+
+
+# route for handling the login page logic
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+            error = 'Invalid Credentials. Please try again.'
+        else:
+            session['logged_in'] = True
+            flash('You were just logged in!')
+            return redirect(url_for('mainpage'))
+    return render_template('login.html', error=error)
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    session.pop('logged_in', None)
+    flash('You were logged out.')
+    return redirect(url_for('welcome'))
+
+
+@app.route('/mainpage')
+@login_required
+def mainpage():
+    return render_template('mainPage.html')
+
+
+# start the server with the 'run()' method
+if __name__ == '__main__':
+    app.run()
+    # app.run(debug=True)
