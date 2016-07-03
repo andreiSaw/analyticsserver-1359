2d2c1e2ea5e6aa354464bcd9d814463f757f4ad2
andreiSaw <ias96@list.ru>
2016-07-03 14:16:35 +0300

    3rd of June 14 16
    fuk uself

---
+++ static/login.js
@@ -0,0 +1,13 @@
+$("#login-button").click(function (event) {
+    var username = ('.username-input').val();
+    var password = ('.password-input').val();
+    if (username == "admin" && password == "admin") {
+        event.preventDefault();
+        $('form').fadeOut(500);
+        $('.wrapper').addClass('form-success');
+        setTimeout(function () {
+            window.location.replace('/main');
+
+        }, 5000);
+    }
+});
